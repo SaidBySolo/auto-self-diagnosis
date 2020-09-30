62f48eddff4c1cfe8d7182922e4901ad744a7345
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-30 13:58:26 +0900

    chore(build): edit typo

--- make_exe.bat
+++ make_exe.bat
@@ -1,8 +1,6 @@
 @ echo Install dependency
 @ pip install -r requirements.txt
 @ echo Install pyinstaller
-@ pip install -r requirements.txt
-@ echo Install pyinstaller
 @ python -m pip install -U pyinstaller
 @ echo Build exe
 @ pyinstaller --onefile --add-binary "chromedriver.exe";"." script.py -i appicon.ico
