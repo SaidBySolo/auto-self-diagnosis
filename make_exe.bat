5875d1cbea1383d49750a472e79e16532d178ff3
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-30 12:49:40 +0900

    chore(build): edit batch file

--- make_exe.bat
+++ make_exe.bat
@@ -1,4 +1,6 @@
 @ echo Install pyinstaller
+@ pip install -r requirements.txt
+@ echo Install pyinstaller
 @ python -m pip install -U pyinstaller
 @ echo Build exe
 @ pyinstaller --onefile --add-binary "chromedriver.exe";"." script.py -i appicon.ico
